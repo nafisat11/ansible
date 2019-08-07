2cf45b5d46658139dc5ca4fdc210ccf4013f26f2
Nafisa Tabassum <32314869+nafisat11@users.noreply.github.com>
2019-08-07 15:55:26 -0600

    Add files via upload
    Figure out how to change duplicate psk entries

--- parsewpasupplicant.py
+++ parsewpasupplicant.py
@@ -1,4 +1,4 @@
-import re
+import more_itertools as m_iter
 from collections import defaultdict
 from itertools import chain
 from pprint import pprint as pp
@@ -50,20 +50,71 @@ def test():
 
 def test2():
     with open("wpa_supplicant.conf") as wpa_conf, open("output.txt", "w+") as outfile:
+        d = defaultdict(list)
+        globs = []
+        network_keys = []
+        network_vals = []
         tmp = []
+        flag = 1
         for line in wpa_conf:
-            vals = ("autoscan", "network", " ")
-            if line.startswith(vals):
-                if not line.strip():
-                    continue
-                if not "#" in line:
-                    line = line.strip()
-                    tmp.append(line)
-        pp(tmp)
+            if not line.strip():
+                continue
+            if not "#" in line:
+                line = line.strip()
+                if line.startswith("network"):
+                    flag = 0
+                elif line.startswith("}"):
+                    flag = 1
+
+                if flag and not line.startswith("}"):
+                    line = tuple(line.strip().split("="))
+                    globs.append(line)
+
+                if not flag and not line.startswith("network"):
+                    key = line.strip().split("=")
+                    network_keys.append(key)
+        #print(network_keys)
+                    #network_vals.append(value)
+
+        #network_keys[network_keys.index("psk")] = "psk_text"
+
+        wpa = dict(tuple(globs))
+        #print(wpa)
+    
+
+        dups = [i for i, x in enumerate(network_keys) if x[0] == "ssid"]
+        dups.remove(0)
+        keys = [tuple(network_keys[i:j]) for i, j in zip(chain([None], dups), chain(dups, [None]))]
+        #vals = tuple([tuple(network_vals[i:j]) for i, j in zip(chain([None], dups), chain(dups, [None]))])
+
+        g = globals()
+        
+        for i, j in zip(chain([None], dups), chain(dups, [None])):
+            g['net_{0}'.format(i)] = network_keys[i:j]
+
+        net_0 = []
+        counter = 0
+        for i, v in enumerate(net_None):
+            if net_None[i][0].count("psk"):
+                count = net_None
+            #total = net_None.count()
+
+
+        wpa.update(net0=dict(net_None))
+        #pp(wpa)
+
+
+            
+
+        
+
+
+                
 
 def test3():
     with open("wpa_supplicant.conf") as wpa_conf:
         tmp = []
+        d = defaultdict(list)
         for line in wpa_conf:
             if not line.strip():
                 continue
@@ -74,8 +125,11 @@ def test3():
         dups = [i for i, x in enumerate(tmp) if "network" in x]
         key_val = [tmp[i:j] for i, j in zip(chain([0], dups), chain(dups, [None]))]
         pp(key_val)
-        #pp(ds)
-        #print(tmp[dups[2]:])
+
+        for i in key_val:
+            for j in i:
+                j = j.split("=")
+                #print(j)
 
 
 
@@ -84,5 +138,5 @@ def test3():
 if __name__ == '__main__':
     #main()
     #print(list(test()))
-    #test2()
-    test3()
+    test2()
+    #test3()
