7279a14c57b83e6bbad181a5c1d2ba7e6b7de096
Nafisa Tabassum <32314869+nafisat11@users.noreply.github.com>
2019-09-23 16:24:10 -0600

    Add files via upload
    Use states to track changes

--- parse_netifaces.py
+++ parse_netifaces.py
@@ -31,32 +31,92 @@ def main():
     header = {"version":"0.0.0"}
     interfaces_json_model = {}
     network_interface_details = OrderedDict()
+    
 
     with open(network_interface_filepath) as interfaces_file:
-        is_interface_stanza = False
-        stanza = []
-        keywords = ("auto", "iface", "post-up", "post-down")
+        interfaces = {}
+        non_auto_ifaces = []
+        auto_ifaces = []
+        inet_ifaces = []
+        inet6_ifaces = []
         for line in interfaces_file:
             line = line.strip().split("#")[0]
 
             #line = re.sub("^(\s*\r?\n){2,}", "\r\n", line)
-            line = re.sub("[ \t]*\n{3,}", "\n\n", line)
+            if not line.strip():
+                continue
 
-            if line.startswith(keywords):
-                interfaces = {}
-                if not interfaces:
-                    stanza.append(interfaces)
-                is_interface_stanza = True
+            line = line.split()
+            if "iface" in line:
+                if "inet" in line:
+                    inet_ifaces.append(line[1])
+                if "inet6" in line:
+                    inet6_ifaces.append(line[1])
 
-            elif not line:
-                is_interface_stanza = False
+                if line[1] in interfaces:
+                    continue
+                interfaces[line[1]] = {}
+                for iface in inet_ifaces:
+                    if iface in interfaces:
+                        interfaces[iface] = {"inet":{}}
+                for iface in inet6_ifaces:
+                    if iface in interfaces and "inet" in interfaces[iface]:
+                        interfaces[iface] = {"inet":{}, "inet6": {}}
+        
 
-            if is_interface_stanza:
-                interfaces[line.split()[0]] = line.split()[1:]
+            if "auto" in line:
+                auto_ifaces.append(line[1])
 
-                
+                #if line[1] in interfaces:
+                    #continue
+                #interfaces[line[1]] = {}
+            for iface_auto_true in auto_ifaces:
+                if iface_auto_true in inet_ifaces:
+                    interfaces[iface_auto_true]["inet"].update({"auto":"true"})
+
+            print(auto_ifaces)
+
+
+
+        pp(interfaces["eth0"])
+
+        
+
+
+
+
+
+
+
+
+
+            #if line.startswith("auto"):
+                #interfaces = {}
+                #if not interfaces:
+                    #stanza.append(interfaces)
+                #is_interface_stanza = True
+                #pass
+
+
+
+            #if is_interface_stanza:
+                #if line == "":
+                    #continue
+                #interfaces[line.split()[0]] = line.split()[1:]
+
+
+
+
+
+                #if line.startswith("auto")
+                    #stanza.append(line)
+                #if line.startswith("auto"):
+                    #stanza.append(line)
+
+
+
     interfaces_json_model["header"] = header
-    interfaces_json_model["interfaces_data"] = network_interface_details
+    interfaces_json_model["interfaces_data"] = interfaces
 
 
     with open("parsed_network_interfaces.json", "w") as json_file:
